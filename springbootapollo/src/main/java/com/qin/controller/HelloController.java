395d9e88c03d3ac971c47a2839a4a7d3a92c90ff
qinchunbang <373413704@qq.com>
2020-01-19 09:10:22 +0800

    添加springboot集成apollo

---
+++ springbootapollo/src/main/java/com/qin/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.qin.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @Value("${server.port}")
+    String port;
+
+    @GetMapping("hi")
+    public String hi(String name) {
+        logger.debug("debug log...");
+        logger.info("info log...");
+        logger.info("warn log...");
+
+        return "hi" + name + ",i am from port:" + port;
+    }
+}
